fd8c9955abb3586d5d3c6f82e45dc3b0ac6c56e7
PedroAz <pdegutty@gmail.com>
2022-08-24 14:59:23 -0300

    crudTest

--- src/main/java/com/pedroapp/crm/model/Cliente.java
+++ src/main/java/com/pedroapp/crm/model/Cliente.java
@@ -2,10 +2,12 @@ package com.pedroapp.crm.model;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class Cliente {
@@ -14,9 +16,13 @@ public class Cliente {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column
 	private String nome;
+	@Column
 	private int idade;
+	@Column
 	private String curso;
+	@Column
 	private String endereco;
 	
 	
---
+++ src/main/resources/Data.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO CLIENTE(nome, idade, curso, endereco) VALUES('Pedro', 41, 'JAVA','Av.Portugal,888');
+INSERT INTO CLIENTE(nome, idade, curso, endereco) VALUES('Paulo', 36, 'Inglês','Lá em Cotia');
+INSERT INTO CLIENTE(nome, idade, curso, endereco) VALUES('Joyce', 37, 'Guitar','Rua das indias');
+INSERT INTO CLIENTE(nome, idade, curso, endereco) VALUES('Joana', 24, 'Botanica','Av.Paraiso, 222');
+INSERT INTO CLIENTE(nome, idade, curso, endereco) VALUES('Joe', 71, 'Chef','Av.Carioca');
+
