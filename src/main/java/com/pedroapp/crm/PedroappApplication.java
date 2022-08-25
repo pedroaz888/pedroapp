0aaf6c8f84456cc6fd1db06504a57a5ec9981cd6
PedroAz <pdegutty@gmail.com>
2022-08-25 15:41:04 -0300

    webSupport

--- src/main/java/com/pedroapp/crm/PedroappApplication.java
+++ src/main/java/com/pedroapp/crm/PedroappApplication.java
@@ -3,7 +3,9 @@ package com.pedroapp.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @EnableCaching
+@EnableSpringDataWebSupport
 @SpringBootApplication
 public class PedroappApplication {
 
