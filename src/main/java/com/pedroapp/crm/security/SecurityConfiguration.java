b247a25e84f55022efd8b0adf493d623a6618407
PedroAz <pdegutty@gmail.com>
2022-09-01 16:14:52 -0300

    filterchain

--- src/main/java/com/pedroapp/crm/security/SecurityConfiguration.java
+++ src/main/java/com/pedroapp/crm/security/SecurityConfiguration.java
@@ -4,45 +4,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.pedroapp.crm.repository.UserRepository;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration{
 	
+
+
 	@Autowired
 	private AutenticacaoService autenticacaoService;
 	
-	@Override
-	public void configure(HttpSecurity http)throws Exception{
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 		
 		http.authorizeHttpRequests()
 		
 			.antMatchers(HttpMethod.GET, "/clientes/").permitAll()
 			.antMatchers(HttpMethod.GET, "/clientes/*").permitAll()
 			.anyRequest().authenticated()
-			.and()
-			.csrf().disable()
-			.headers().frameOptions().sameOrigin()
-			.and().formLogin();
+			.and().csrf().disable()
+			.headers().frameOptions().sameOrigin();
+			
+			
+
+			return http.build();
 		
 }
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-		auth.userDetailsService(autenticacaoService)
-			.passwordEncoder(new BCryptPasswordEncoder());
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
-	
 
+	@Bean
+	protected AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+
+	}
 }
 
